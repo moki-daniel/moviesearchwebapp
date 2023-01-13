d731353a8e4d8ee34a2a4b168daa633bea7a2248
moki-daniel <mokidnl@gmail.com>
2023-01-13 09:43:10 +0300

    connected my client with server (rapidAPI)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
 
-function App() {
+const App = () =>  {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '1a836a595bmshba6f885e58262c1p1f60ccjsn0a0a266111f4',
+      'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+    // .then(response => response.json())
+    .then(response => {
+      console.log(response.json()); 
+    })
+    .catch(err => console.error(err));
+
   return (
     <div className="App">
      
