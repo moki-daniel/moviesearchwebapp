1c398b5fe45b16d7f13484b7d9ba53ad9e8b5964
moki-daniel <mokidnl@gmail.com>
2023-01-13 10:47:50 +0300

    updated state to fetch api objets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 const App = () =>  {
+  const [endPoint, setEndPoints] = useState('')
+  const [container, setContainer] = useState([])
+   
   const options = {
     method: 'GET',
     headers: {
@@ -9,15 +13,33 @@ const App = () =>  {
     }
   };
   
-  fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-    // .then(response => response.json())
-    .then(response => {
-      console.log(response.json()); 
+  fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=+${endPoint}`, options)
+
+    //.then(response => response.json())
+  .then(response => {
+    return response.json()
+    // .then(response => {
+    //   console.log(response.json()); 
+    })
+    .then(data => {
+      setContainer(data)
     })
     .catch(err => console.error(err));
 
+    const onChangeHandler = (e) => {
+      setEndPoints(e.target.value)
+    }
+
+    const submitHandler = e => {
+      e.preventDefault()
+    }
+
   return (
     <div className="App">
+      <form onsubmit={submitHandler}>
+        <input type = "text" value={endPoint} onChange={onChangeHandler}/>
+        <button type="submit">Request</button>
+      </form>
      
     </div>
   );
