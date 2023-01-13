25a0814fcb3746a23890bff9271726bf897e2d9d
moki-daniel <mokidnl@gmail.com>
2023-01-13 11:44:21 +0300

    style the fetched objects

--- src/App.js
+++ src/App.js
@@ -5,17 +5,13 @@ const App = () =>  {
   const [endPoint, setEndPoints] = useState('');
   const [container, setContainer] = useState([]);
   const [finalPoint, setFinalPoint] = useState('');
-
+  
   useEffect(() => {
     fetchObject()
-
   }, [finalPoint])
 
   const fetchObject = () => {
-
-  
-   
-  const options = {
+    const options = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Key': '1a836a595bmshba6f885e58262c1p1f60ccjsn0a0a266111f4',
@@ -24,23 +20,19 @@ const App = () =>  {
   };
   
   fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=+${endPoint}`, options)
-
-    //.then(response => response.json())
   .then(response => {
     return response.json()
-    // .then(response => {
-    //   console.log(response.json()); 
     })
     .then(data => {
       setContainer(data.d)
     })
     .catch(err => console.error(err));
   }
-
-    const onChangeHandler = (e) => {
+  
+  const onChangeHandler = (e) => {
       setEndPoints(e.target.value)
     }
-
+    
     const submitHandler = e => {
       e.preventDefault()
       setFinalPoint(endPoint)
@@ -52,16 +44,18 @@ const App = () =>  {
         <input type = "text" value={endPoint} onChange={onChangeHandler}/>
         <button type="submit">Request</button>
       </form>
-      {container.map((item, index) => {
-        return(
+      <div clasName ="element">
+        {container.map((item, index) => {
+          return(
           <div key={index} className='element-div'>
             <img src={item.i.imageUrl} alt="" />
             <p>{item.l}</p>
-          </div>
-          )}
-        )}
-     
-    </div>
+            <p>{item.s}</p>
+            </div>
+            )}
+            )}
+            </div>
+            </div>
   );
 }
 
